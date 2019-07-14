49d8b6f8d9ee43f16c70b72ecadcd38b51351d56
Hagen Hiller <hagen@weloveapps.de>
2019-07-14 19:04:48 +0200

    added volume controller

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require( 'express' );
 const app = express();
 
 let InfoController = require('./info/InfoController');
-app.use('/export', InfoController);
+app.use('/info', InfoController);
 
 let VolumeController = require('./volume/VolumeController');
-app.use('/export', VolumeController);
+app.use('/volume', VolumeController);
 
 module.exports = app;
--- volume/ImportController.js
+++ volume/ImportController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-const url = require('url');
+const {exec} = require('child_process');
 
 router.use(bodyParser.urlencoded({extended: false}));
 router.use(bodyParser.json());
@@ -9,20 +9,131 @@ router.use(bodyParser.json());
 const fs = require('fs');
 const util = require('util');
 
-router.get('/', async function (req, res) {
+router.post('set/', async function (req, res) {
     try {
-        const timestamp = new Date();
-
-        const file_name = `dump-${timestamp.getFullYear()}-${timestamp.getMonth()}-${timestamp.getDay()}.json`;
-        fs.writeFile(file_name, JSON.stringify(req.body), (err) => {
-            console.log(err);
-        });
-
-        res.status(200).send({success: true});
+        const value = req.body.value;
+        if (getSystemVolume(value)) return res.status(200).send({success: true});
+        return res.status(500).send({success: false, message: "Error executing command!"});
     } catch (error) {
         console.error(error);
         return res.status(500).send({result: {message: "There was an error importing the data!"}});
     }
 });
+
+router.post('set/', async function (req, res) {
+    try {
+        const value = req.body.value;
+        if (setSystemVolume(value)) return res.status(200).send({success: true});
+        return res.status(500).send({success: false, message: "Error executing command!"});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({result: {message: "There was an error importing the data!"}});
+    }
+});
+
+router.post('increment/', async function (req, res) {
+    try {
+        if(setRelativeSystemVolume('+', req.body.precise)) {
+            return res.status(200).send({success: true});
+        }
+        return res.status(500).send({success: true});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({result: {message: "There was an error importing the data!"}});
+    }
+});
+
+router.post('decrement/', async function (req, res) {
+    try {
+        if(setRelativeSystemVolume('-', req.body.precise)) {
+            return res.status(200).send({success: true});
+        }
+        return res.status(500).send({success: true});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({result: {message: "There was an error importing the data!"}});
+    }
+});
+
+
+router.post('mute/', async function (req, res) {
+    try {
+        if(muteSystem(req.body.mute)){
+            return res.status(200).send({success: true});
+        }
+        return res.status(500).send({success: true});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({result: {message: "There was an error importing the data!"}});
+    }
+});
+
+
+function setSystemVolume(volume) {
+    const command = `amixer set Speaker ${volume}`;
+    exec(command, (err, stdout, stderr) => {
+        if (err) {
+            console.log("Error executing:", command);
+            return false;
+        }
+
+        // the *entire* stdout and stderr (buffered)
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        return true
+    });
+    return false;
+}
+
+function setRelativeSystemVolume(prefix, precise) {
+    let value = precise ? '2%' : '5%';
+    const command = `amixer set Speaker ${value}${prefix}`;
+    exec(command, (err, stdout, stderr) => {
+        if (err) {
+            console.log("Error executing:", command);
+            return false;
+        }
+
+        // the *entire* stdout and stderr (buffered)
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        return true
+    });
+    return false;
+}
+
+function getSystemVolume() {
+    const command = `amixer get Speaker`;
+    exec(command, (err, stdout, stderr) => {
+        if (err) {
+            console.log("Error executing:", command);
+            return false;
+        }
+
+        // the *entire* stdout and stderr (buffered)
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        return true
+    });
+    return false;
+}
+
+function muteSystem(mute) {
+    const value = mute ? 'mute' : 'unmute';
+    const command = `amixer set Speaker ${value}`;
+    exec(command, (err, stdout, stderr) => {
+        if (err) {
+            console.log("Error executing:", command);
+            return false;
+        }
+
+        // the *entire* stdout and stderr (buffered)
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        return true
+    });
+    return false;
+}
+
 
 module.exports = router;
