af776878d3739b738f5eb98e49a6ccdbfc3ede05
Hagen Hiller <hagen@weloveapps.de>
2019-07-14 20:36:25 +0200

    removed config check

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const config = require( 'config' );
+const config = require('config');
 
-let app = require( './app' );
-let port = process.env.PORT || config.get( 'server.port' );
+let app = require('./app');
+// let port = process.env.PORT || config.get( 'server.port' );
 
-let server = app.listen( port, function () {
-    console.log( 'Express server listening on port ' + port );
-} );
+// let server = app.listen( port, function () {
+let server = app.listen(42001, function () {
+    console.log('Express server listening on port ' + port);
+});
