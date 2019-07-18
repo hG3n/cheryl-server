9ee4dc6fac94da4cde28e0f391bdd2f26d1c8c07
Hagen Hiller <hagen@weloveapps.de>
2019-07-18 23:15:23 +0200

    ting

--- volume/VolumeController.js
+++ volume/VolumeController.js
@@ -13,19 +13,10 @@ router.get('/', async function (req, res) {
     try {
         const value = req.body.value;
         let result = await getSystemVolume(value);
-        console.log(result);
 
-        //     .then(
-        //     (value) => {
-        //         if (value)
-        //             console.log("whoooooooo");
-        //             return res.status(200).send({success: true, message: value});
-        //         return res.status(500).send({success: false, message: "Error executing command!"});
-        //     },
-        //     () => {
-        //         return res.status(500).send({success: false, message: "Error executing Command"});
-        //     }
-        // );
+        if (result) return res.status(200).send({success: true, message: value});
+        return res.status(500).send({success: false, message: "Error executing command!"});
+        
     } catch (error) {
         console.error(error);
         return res.status(500).send({result: {message: "There was an error importing the data!"}});
