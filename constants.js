2f741085d686ba2e7d89d41f0e182eda6e6c3871
Hagen Hiller <hagen@weloveapps.de>
2019-07-19 00:54:36 +0200

    ting

--- constants.js
+++ constants.js
@@ -9,47 +9,47 @@ const constants = {
             },
             {
                 name: "63 Hz",
-                property: "00. 63 Hz",
+                property: "01. 63 Hz",
                 position: 1
             },
             {
                 name: "125 Hz",
-                property: "00. 125 Hz",
+                property: "02. 125 Hz",
                 position: 2
             },
             {
                 name: "250 Hz",
-                property: "00. 250 Hz",
+                property: "03. 250 Hz",
                 position: 3
             },
             {
                 name: "500 Hz",
-                property: "00. 500 Hz",
+                property: "04. 500 Hz",
                 position: 4
             },
             {
                 name: "1 kHz",
-                property: "00. 1 kHz",
+                property: "05. 1 kHz",
                 position: 5
             },
             {
                 name: "2 kHz",
-                property: "00. 2 kHz",
+                property: "06. 2 kHz",
                 position: 6
             },
             {
                 name: "4 kHz",
-                property: "00. 4 kHz",
+                property: "07. 4 kHz",
                 position: 7
             },
             {
                 name: "8 kHz",
-                property: "00. 8 kHz",
+                property: "08. 8 kHz",
                 position: 8
             },
             {
                 name: "16 kHz",
-                property: "00. 16 kHz",
+                property: "09. 16 kHz",
                 position: 9
             },
         ]
