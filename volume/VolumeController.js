13721dc4f9347c18a2b5d8964019162f11b32ada
Hagen Hiller <hagen@weloveapps.de>
2019-07-19 12:57:05 +0200

    change

--- volume/VolumeController.js
+++ volume/VolumeController.js
@@ -97,6 +97,7 @@ function setSystemVolume(volume) {
 
 function setRelativeSystemVolume(prefix, precise) {
     let value = precise ? '2%' : '5%';
+    console.log('vol commands', constants.commands.volume);
     const command = constants.commands.volume.set + ` ${value}${prefix}`;
     console.log('cmd', command);
     return new Promise((resolve, reject) => {
